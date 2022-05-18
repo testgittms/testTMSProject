a0a2018662f35db14be017702f9646850856c354
Andrey_Trofimov <Andrey.Trofimov@life.com.by>
2022-05-18 19:59:14 +0300

    add tabBar

---
+++ TestGITProject/TestGITProject/TabBar/Tab1/Tab1ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Tab1ViewController.swift
+//  TestGITProject
+//
+//  Created by lifetech on 18.05.22.
+//
+
+import UIKit
+
+class Tab1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
